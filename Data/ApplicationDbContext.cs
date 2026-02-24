85fccf2c3cad7725ca9fe1620733649cc498652a
Eric Ryan Montgomery <ermontgomery1@crimson.ua.edu>
2026-02-24 14:04:28 -0600

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Spring2026_Project2_ermontgomery1.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
