4d844b3fdc69d6079572b5e5088bdb4ad6b6c953
Eric Ryan Montgomery <ermontgomery1@crimson.ua.edu>
2026-02-28 14:41:40 -0600

    Ready to publish

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Spring2026-Project2-ermontgomery1
+# Googolplex Search
+This project is a mock Google search engine built using Javascript, HTML, and CSS. It provides the user the ability to search the internet, perform a random search with the "I'm Feeling Lucky" button, and get the current time, as well as change the background by clicking on the Googolplex title. The search results are generated using Serper API, which provides search results based on the user's query.
+
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,84 @@
-﻿//When the document is ready, we set up event listeners for the search button and the search text input.
+﻿//These are the background images to load
+const bgImages = [
+    '../backgrounds/NewYork.jpg',
+    '../backgrounds/Space.jpg',
+    '../backgrounds/WinterLamp.jpg'
+];
+
+//This is an index variable to determine which background is in use
+let currentBgIndex = parseInt(localStorage.getItem('userBgIndex')) || 0;
+
+//This function applies a new background using the index provided
+function applyBackground(index) {
+    $('body').css({
+        'background-image': `url('${bgImages[index]}')`,
+        'background-size': 'cover'
+    });
+}
+
+//When the page loads, we apply the background based on the saved index in localStorage (or default to 0)
+applyBackground(currentBgIndex);
+
+//Preload the background images to ensure smooth transitions when switching backgrounds
+bgImages.forEach(imagePath => {
+    const img = new Image();
+    img.src = imagePath;
+});
+
+//This is an array to hold fun search terms for the "I'm Feeling Lucky" button
+let funTerms = [];
+
+//We load the fun search terms from a local JSON file. 
+//This file should contain an array of fun search queries.
+fetch('../js/searches.json')
+    .then(response => response.json())
+    .then(data => {
+        funTerms = data;
+    })
+    .catch(error => {
+        console.error("Error loading fun words:", error);
+    });
+
+//This sets up a click event listener on the title to change the background image.
+$('.EngineTitle').click(function () {
+    currentBgIndex = (currentBgIndex + 1) % bgImages.length;
+
+    localStorage.setItem('userBgIndex', currentBgIndex);
+
+    applyBackground(currentBgIndex);
+});
+
+//This is the event listener for the time button.
+$('#timeButton').click(function () {
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    const timeString = `${hours}:${minutes} ${ampm}`;
+
+    $('#timeContent').text(`The current time is: ${timeString}`);
+
+    $('#timeDialog').dialog({
+        modal: true,
+        buttons: {
+            Ok: function () {
+                $(this).dialog("close");
+            }
+        }
+    });
+});
+
+//This is the event listener for the "I'm Feeling Lucky" button. It triggers a search with a random term.
+$('#luckyButton').click(function () {
+    doSearch(true);
+});
+
+//When the document is ready, we set up event listeners for the search button and the search text input.
 //Any button with this listener will trigger the doSearch function when clicked.
 $('#searchButton').click(function () {
     doSearch();
@@ -6,7 +86,7 @@ $('#searchButton').click(function () {
 
 //This is an alternative to the search button. If the user presses enter in the search box,
 //then the doSearch function will also be triggered.
-$('#searchText').keyup(function (e) {
+$('#query').keyup(function (e) {
     const ENTER_KEY_CODE = 13;
     if (e.keyCode === ENTER_KEY_CODE) {
         doSearch();
@@ -15,15 +95,26 @@ $('#searchText').keyup(function (e) {
 
 //This is the function that does all the hard work
 //of searching, parsing, and formatting the search results.
-function doSearch() {
+function doSearch(isLucky = false) {
+
     //This grabs the text the user wrote in the search box
     //It also trims any extra whitespace at the beginning or the end
     //of the search query.
-    const searchVal = $('#searchText').val().trim();
+    let searchVal = $('#query').val().trim();
 
-    //If the query is empty return and do not make a call.
+    // If empty and it's a Lucky click, provide a random fun search!
+    if (isLucky === true) {
+        if (funTerms.length === 0) {
+            console.warn("Fun terms not loaded yet.");
+            return;
+        }
+        searchVal = ''; // Now searchVal is ""
+        searchVal = funTerms[Math.floor(Math.random() * funTerms.length)];
+        $('#query').val(searchVal);
+    }
+
     if (searchVal.length === 0) {
-        return;
+        return; // Still exit if it's a normal search and empty
     }
 
     //This is the AJAX call to the API. It sends the search query and gets back the results.
@@ -44,12 +135,19 @@ function doSearch() {
     })
     .done(function (data) {
 
+        if (isLucky && data.organic && data.organic.length > 0) {
+            window.location.href = data.organic[0].link;
+            return; // Stop execution so we don't render the rest of the page
+        }
+
+        $('body').addClass('search-active');
+
         //The resultsContainer is the div where we will put all the search results. We first clear it out.
         const resultsContainer = $('#searchResults');
         resultsContainer.empty();
 
         //This updates the header above the search results to show what the user searched for.
-        $('#searchResultsHeader').text(`Results for "${$('#searchText').val().trim()}":`);
+        $('#searchResultsHeader').text(`Results for "${$('#query').val().trim()}":`);
 
         //Knowledge Graph
         if (data.knowledgeGraph) {
@@ -58,7 +156,7 @@ function doSearch() {
             const kg = data.knowledgeGraph;
 
             //Creates a new html div element in memory.
-            const kgDiv = $('<div>').addClass('knowledge-graph');
+            const kgDiv = $('<div>').addClass('ui-widget-header knowledge-graph');
 
             // Create a wrapper for the text content so it stays together on the left
             const textWrapper = $('<div>').addClass('kg-text-wrapper');
@@ -118,7 +216,7 @@ function doSearch() {
 
                 //Finally, we append this entire search result div to our results container, and add a line break after it.
                 resultsContainer.append(resultDiv);
-                resultsContainer.append($('<br>'));
+                //resultsContainer.append($('<br>'));
             });
         }
 
@@ -159,8 +257,10 @@ function doSearch() {
 
             //Create a new div for the "Related Searches" section
             const relatedDiv = $('<div>')
-                .addClass('ui-widget-content related-searches');
+                .addClass('ui-widget-content related-searches-header');
             relatedDiv.append($('<h4>').text('Related Searches'));
+
+            const relatedButtonsDiv = $('<div>').addClass('related-search-wrapper');
 
             //For each related search, we create a button that the user can click to perform that search.
             data.relatedSearches.forEach(item => {
@@ -171,13 +271,14 @@ function doSearch() {
                     .addClass('related-search-button')
                     .text(item.query)
                     .on('click', function () {
-                        $('#searchText').val(item.query);
+                        $('#query').val(item.query);
                         doSearch();
                     });
 
                 //We append this button to our "Related Searches" div.
-                relatedDiv.append(button);
+                relatedButtonsDiv.append(button);
             });
+            relatedDiv.append(relatedButtonsDiv);
 
             //Finally, we append the entire "Related Searches" div to our results container.
             resultsContainer.append(relatedDiv);
